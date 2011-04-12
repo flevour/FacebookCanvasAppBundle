b4774fe9d6b29cf243d253d1bd01cdc90e5b8d75
caefer <caefer@ical.ly>
2011-04-12 09:33:44 +0200

    listener will now redirect to referer by default

--- Security/Firewall/FacebookAppListener.php
+++ Security/Firewall/FacebookAppListener.php
@@ -2,18 +2,34 @@
 
 namespace Caefer\FacebookAppBundle\Security\Firewall;
 
-use Caefer\FacebookAppBundle\Security\Authentication\Token\FacebookAppToken;
 use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener;
-
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Session\SessionAuthenticationStrategyInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Caefer\FacebookAppBundle\Security\Authentication\Token\FacebookAppToken;
 
 /**
  * Facebook authentication listener.
  */
 class FacebookAppListener extends AbstractAuthenticationListener
 {
+    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager, SessionAuthenticationStrategyInterface $sessionStrategy, $providerKey, array $options = array(), AuthenticationSuccessHandlerInterface $successHandler = null, AuthenticationFailureHandlerInterface $failureHandler = null, LoggerInterface $logger = null, EventDispatcherInterface $dispatcher = null)
+    {
+        $options['use_referer'] = $options['use_referer'] ?: true;
+        parent::__construct($securityContext, $authenticationManager, $sessionStrategy, $providerKey, $options, $successHandler, $failureHandler, $logger, $dispatcher);
+    }
+
     protected function attemptAuthentication(Request $request)
     {
-        return $this->authenticationManager->authenticate(new FacebookAppToken());
+        if (null === $token = $this->authenticationManager->authenticate(new FacebookAppToken())) {
+            return null;
+        }
+
+        return $token;
     }
 }
