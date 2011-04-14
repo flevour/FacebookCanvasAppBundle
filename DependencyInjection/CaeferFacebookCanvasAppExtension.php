039341c31c499090d6567dfe25d6b27ab7b13542
caefer <caefer@ical.ly>
2011-04-14 21:37:47 +0200

    Added support for multiple Facebook apps

--- DependencyInjection/CaeferFacebookCanvasAppExtension.php
+++ DependencyInjection/CaeferFacebookCanvasAppExtension.php
@@ -6,14 +6,43 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class CaeferFacebookCanvasAppExtension extends Extension
 {
     public function load(array $config, ContainerBuilder $container)
     {
-        $container->setParameter('caefer_facebook_canvas_app.api.file', $config[0]['api']['file']);
-        $container->setParameter('caefer_facebook_canvas_app.api.app_id', $config[0]['api']['app_id']);
-        $container->setParameter('caefer_facebook_canvas_app.api.secret', $config[0]['api']['secret']);
+        if (isset($config[0]['apps'])) {
+            $first = true;
+            foreach ($config[0]['apps'] as $name => $app) {
+
+                $definition = new Definition();
+                $definition->setClass('Facebook');
+                $definition->setFile($config[0]['file']);
+                $definition->setArguments(array(array(
+                    'appId'  => $app['app_id'],
+                    'secret' => $app['secret'],
+                    'cookie' => true,
+                )));
+
+                $container->setDefinition('caefer_facebook_canvas_app.app.'.$name, $definition);
+
+                $container->setParameter('caefer_facebook_canvas_app.app.'.$name.'.app_id', $app['app_id']);
+                $container->setParameter('caefer_facebook_canvas_app.app.'.$name.'.secret', $app['secret']);
+
+                if ($first) {
+                    $container->setParameter('caefer_facebook_canvas_app.api.file',   $config[0]['file']);
+                    $container->setParameter('caefer_facebook_canvas_app.api.app_id', $app['app_id']);
+                    $container->setParameter('caefer_facebook_canvas_app.api.secret', $app['secret']);
+                    $first = true;
+                }
+            }
+        } else {
+
+            $container->setParameter('caefer_facebook_canvas_app.api.file',   $config[0]['api']['file']);
+            $container->setParameter('caefer_facebook_canvas_app.api.app_id', $config[0]['api']['app_id']);
+            $container->setParameter('caefer_facebook_canvas_app.api.secret', $config[0]['api']['secret']);
+        }
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('security.xml');
--- README.md
+++ README.md
@@ -26,6 +26,36 @@ Then specify where your Facebook PHP-SDK is located and your app id and secret.
             app_id: xxxxx
             secret: xxxxx
 
+In your application you can get a service instance of that app like this.
+
+    <?php
+
+    $fb = $container->get('caefer_facebook_canvas_app.api');
+
+If you need to access multiple Facebook apps in your project you can define them like this.
+
+    caefer_facebook_canvas_app:
+        file:   "%kernel.root_dir%/../vendor/facebook/src/facebook.php"
+        apps:
+            app_1:
+                app_id: xxxxx
+                secret: xxxxx
+            app_2:
+                app_id: yyyyy
+                secret: yyyyy
+
+You can then get your services like this.
+
+    <?php
+
+    $app_1 = $container->get('caefer_facebook_canvas_app.app.app_1');
+    $app_2 = $container->get('caefer_facebook_canvas_app.app.app_2');
+
+    // this will still work and act as an alias to the first app configured (app_1)
+    $fb = $container->get('caefer_facebook_canvas_app.api');
+
+Each Facebook app will automatically get its own session cookie ('fbs_' + <app_id>).
+
 Also add the following to your `app/config/routing.yml`.
 
     caefer:
